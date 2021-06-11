896bd16e930d6e5108695364a2e848806988aed6
jLuseno161 <joyluseno2@gmail.com>
2021-06-11 22:06:27 +0300

    add sql url to config.py

--- config.py
+++ config.py
@@ -1,15 +1,32 @@
 import os
 
 class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    @staticmethod
+    def init_app(app):
+        pass
     pass
 
 class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
     pass
 
 class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jluseno@localhost/pitch_test'
+
     pass
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jluseno@localhost/pitches'
+
     DEBUG = True
     pass
 
