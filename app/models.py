b1fcc86cf5e7eb43695d714f3fcd354bcfc6de2a
jLuseno161 <joyluseno2@gmail.com>
2021-06-11 23:11:26 +0300

    git validate user password & fetch user pitches & comments

--- app/models.py
+++ app/models.py
@@ -20,3 +20,23 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         db.session.commit()
 
+    @property
+    def password(self):
+        raise AttributeError("You cant always get it right")
+
+    @password.setter
+    def password(self, password):
+        self.pass_locked = generate_password_hash(password)
+
+    def verify_pass(self, password):
+        return check_password_hash(self.pass_locked, password)
+
+    def get_user_pitches(self):
+        user = User.query.filter_by(id=self.id).first()
+        return user.pitches
+
+    def get_user_comments(self):
+        user = User.query.filter_by(id=self.id).first()
+        return user.comments
+
+
